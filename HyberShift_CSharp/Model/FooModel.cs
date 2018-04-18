3392f0ba2c1129eea46c73261b47f02c03bb6280
helloqwert12 <tranminhquan1201@gmail.com>
2018-04-18 21:11:49 +0700

    Init project structure + example

---
+++ HyberShift_CSharp/Model/FooModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyberShift_CSharp.Model
+{
+    public class FooModel
+    {
+        // attribute of model
+        private string attribute1;
+        private double attribute2;
+        private bool isFoo;
+
+        // getter and setter
+        public string Attribute1
+        {
+            get { return attribute1; }
+            set { attribute1 = value; }
+        }
+
+        public double Attribute2
+        {
+            get { return attribute2; }
+            set { attribute2 = value; }
+        }
+
+        public bool IsFoo
+        {
+            get { return isFoo; }
+            set { isFoo = value; }
+        }
+
+        //methods of model
+        public void Method1()
+        {
+
+        }
+
+        public double Method2()
+        {
+            return 0f;
+        }
+
+        public bool IsValidAttribute1() //example about data validation
+        {
+            return true;
+        }
+
+        // others method ...
+    }
+}
---
+++ HyberShift_CSharp/ViewModel/FooViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using HyberShift_CSharp.Model;  // import namespace of model
+using Prism.Commands;
+
+namespace HyberShift_CSharp.ViewModel
+{
+    class FooViewModel : INotifyPropertyChanged // this interface is for automatically change property on the view
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private FooModel obj;
+        private DelegateCommand objCommand;
+
+        // constructor
+        public FooViewModel()
+        {
+            obj = new FooModel();
+            objCommand = new DelegateCommand(obj.Method1, obj.IsValidAttribute1);  // delegate to Method1 as Execute() and IsValidAttribute1 as CanExecute()
+        }
+
+        // For example, this method is invoked by a button from view
+        // and use Method1 of obj (from Model)
+        public void Method1()   
+        {
+            obj.Method1();
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("label1"));  // this will automatically update label1
+            }
+        }
+        
+    }
+}
