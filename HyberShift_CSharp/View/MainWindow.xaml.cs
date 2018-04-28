4456923ee1d1f35afa92a465ceaa1af39d109faa
QuanDao <daonguyenquan@hotmail.com>
2018-04-28 18:45:54 +0700

    Menubar

--- HyberShift_CSharp/View/MainWindow.xaml.cs
+++ HyberShift_CSharp/View/MainWindow.xaml.cs
@@ -1,17 +1,37 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
 
-namespace HyberShift_CSharp
+namespace HyberShift_CSharp.View
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        bool _stateClosed = true;
+
         public MainWindow()
         {
             InitializeComponent();
             //var socketAPI = SocketAPI.GetInstance();
             //socketAPI.Connect();
+        }
+
+        private void ButtonMenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_stateClosed)
+            {
+                Storyboard sb = this.FindResource("OpenMenu") as Storyboard;
+                sb.Begin();
+            }
+            else
+            {
+                Storyboard sb = this.FindResource("CloseMenu") as Storyboard;
+                sb.Begin();
+            }
+
+            _stateClosed = !_stateClosed;
         }
     }
 }
