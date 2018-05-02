2b0c140f04b0cc30a8c2f663da78ca275e1834b3
helloqwert12 <tranminhquan1201@gmail.com>
2018-05-03 00:50:59 +0700

    +Add VoiceAPI class +Add LogOutput method (Debug class)

--- HyberShift_CSharp/Utilities/Debug.cs
+++ HyberShift_CSharp/Utilities/Debug.cs
@@ -8,11 +8,14 @@ namespace HyberShift_CSharp.Utilities
 {
     public static class Debug
     {
-        //private static StringBuilder sb = new StringBuilder();
-
         public static void Log(string content)
         {
             File.AppendAllText("log.txt", DateTime.Now + ": " + content + Environment.NewLine);
+        }
+
+        public static void LogOutput(string content)
+        {
+            System.Diagnostics.Debug.WriteLine(content);
         }
     }
 }
