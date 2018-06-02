9d90f2e10950894c01e30df3c267f6e3e0fe1c3d
Kiet Vo <geforce2409@gmail.com>
2018-06-02 19:29:24 +0700

    fix minor bug

--- HyberShift_CSharp/ViewModel/BoardViewModel.cs
+++ HyberShift_CSharp/ViewModel/BoardViewModel.cs
@@ -181,7 +181,9 @@ namespace HyberShift_CSharp.ViewModel
         public void OpenPresentationSlide()
         {
             string path = dialogService.OpenFile("Choose presentation file", "All Files|*.pptx;*.ppt|Presentation (.pptx ,.ppt)|*.pptx;*.ppt");
-            
+
+            if (path == "")
+                return;
 
             ThreadStart starter = () =>
             {
