ff409ecfbb09f6652078f875a09b07f89eb57a7b
QuanDao <daonguyenquan@hotmail.com>
2018-06-04 15:16:42 +0700

    update Avatar img in Sign up

--- HyberShift_CSharp/Properties/Resources.Designer.cs
+++ HyberShift_CSharp/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace HyberShift_CSharp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap img_placeholder {
+            get {
+                object obj = ResourceManager.GetObject("img_placeholder", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap login_page {
             get {
                 object obj = ResourceManager.GetObject("login_page", resourceCulture);
